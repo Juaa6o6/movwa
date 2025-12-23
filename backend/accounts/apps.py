d7d31aa5e83574c9c55ac3ec1f3759d8858e9f4b
Park Ju Hyeong <owo__omo@naver.com>
2025-12-24 01:20:01 +0900

    ✨ Feat: User 모델 확장, Profile, Follow 모델 및 Signal 구현

--- backend/accounts/apps.py
+++ backend/accounts/apps.py
@@ -1,6 +1,9 @@
 from django.apps import AppConfig
 
-
 class AccountsConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'accounts'
+    
+    def ready(self):
+        """앱이 준비되면 실행되는 메서드"""
+        import accounts.signals  # 시그널 등록!
