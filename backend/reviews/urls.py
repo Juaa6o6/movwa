8a7d0cee9930454690dfc79d4db2debdd09b4370
박주형 <owo__omo@naver.com>
2025-12-23 12:00:12 +0900

    ✨ Feat: Review 및 ReviewLike 모델 구현

---
+++ backend/reviews/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from . import views
+
+app_name = 'reviews'
+
+urlpatterns = [
+    # 리뷰 관련 URL 패턴을 추후 추가
+    # 예: path('<uuid:movie_id>/', views.ReviewListCreateView.as_view(), name='review-list'),
+]
