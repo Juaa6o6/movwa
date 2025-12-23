295b5fb26e7d5778a0954192f5ea38ec03e26dce
Park Ju Hyeong <owo__omo@naver.com>
2025-12-24 07:26:25 +0900

    ✨ Feat: 프로필 이미지 지원을 위한 Pillow 및 기본 프로필 이미지 추가

--- backend/config/urls.py
+++ backend/config/urls.py
@@ -16,6 +16,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 from drf_spectacular.views import SpectacularAPIView, SpectacularRedocView, SpectacularSwaggerView
 
 urlpatterns = [
@@ -38,4 +40,9 @@ urlpatterns = [
     path('api/docs/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
     # 3. Redoc (다른 스타일의 문서)
     path('api/redoc/', SpectacularRedocView.as_view(url_name='schema'), name='redoc'),
+
 ]
+
+# 개발 환경에서 미디어 파일 서빙 설정
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
