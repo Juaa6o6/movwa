a22eb2c28917f415ed4ea80160e0d9068980a7af
박주형 <owo__omo@naver.com>
2025-12-24 17:50:09 +0900

    ✨ Feat: 박스오피스 API 엔드포인트 구현

--- backend/movies/urls.py
+++ backend/movies/urls.py
@@ -13,7 +13,10 @@ from .views import (
     # 마이페이지/목록 조회 View
     MySaveListView,
     MyRatedListView,
-    TodaysPickListView
+    TodaysPickListView,
+    
+    # 큐레이션 View
+    BoxOfficeView,
 )
 
 app_name = 'movies'
@@ -23,6 +26,7 @@ urlpatterns = [
     # 1. 영화 정보 조회 (Public/General)
     path('', MovieListView.as_view(), name='movie-list'),
     path('search/', MovieSearchView.as_view(), name='movie-search'),  # 반드시 <uuid:pk>/ 보다 위에!
+    path('boxoffice/', BoxOfficeView.as_view(), name='boxoffice'),
     path('<uuid:pk>/', MovieDetailView.as_view(), name='movie-detail'),
 
 
